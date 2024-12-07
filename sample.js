e2668f7e9ecffe71a2bf0be6befcd4e68e709bb3
a-vasanth-kumar <55576815+a-vasanth-kumar@users.noreply.github.com>
2024-12-08 00:09:07 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,30 +1,18 @@
-const express = require('express');
 const mysql = require('mysql');
-const app = express();
 
-app.get('/user', (req, res) => {
-    const username = req.query.username;
-    const password = req.query.password;
-
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'mydatabase'
-    });
-
-    connection.connect();
-
-    // GOOD: Use parameterized queries to prevent SQL injection
-    const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
-    connection.query(query, [username, password], (error, results) => {
-        if (error) throw error;
-        res.send(results);
-    });
-
-    connection.end();
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // BAD: Hardcoded username
+    password: 'password', // BAD: Hardcoded password
+    database: 'mydatabase'
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Connected as id ' + connection.threadId);
 });
+
+connection.end();
